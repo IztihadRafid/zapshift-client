8e21079236627bea0a5ea152561dcf4fedf59c97
Iztihad Rafid <iztihadrafid10@gmail.com>
2026-04-29 16:25:40 +0600

    installed tailwind, react, shadcn, router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+  <>
+   <h1>hello</h1>
+   <Button>a</Button></>
+  )
+}
+
+export default App
